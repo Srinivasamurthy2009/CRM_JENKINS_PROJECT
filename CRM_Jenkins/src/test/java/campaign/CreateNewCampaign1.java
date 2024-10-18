3f8f2c26a19ab961776b40ee6f6031928e0ed526
Srinivasa Murthy <Srinivasa Murthy@DESKTOP-8NOR5L3>
2024-10-18 20:48:17 +0530

    New Commit for class

---
+++ CRM_Jenkins/src/test/java/campaign/CreateNewCampaign1.java
@@ -0,0 +1,14 @@
+package campaign;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class CreateNewCampaign1 {
+
+		@Test
+		
+		public void createOrg() {
+			Reporter.log("Created Campaign Successfully", true);
+		}
+
+}
